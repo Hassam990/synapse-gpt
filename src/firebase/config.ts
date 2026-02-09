45107ffdbbf42c8038c7ffd6a62e89c06175b78f
Jahanzaib Azam <hassamazam999@gmail.com>
2026-02-09 19:55:49 +0000

    Add Firebase SDK
    Use npm
    
    Use a <script> tag
    If you're already using np

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-4815316560-21c29",
-  "appId": "1:816912441965:web:46c86a9f35ae5af9379564",
-  "apiKey": "AIzaSyBO5d3ZbS1M8BoQqyiVvc0JB5ZKVFfjhas",
-  "authDomain": "studio-4815316560-21c29.firebaseapp.com",
-  "measurementId": "G-EHJDDNL4SJ",
-  "messagingSenderId": "816912441965"
+  apiKey: "AIzaSyD6z2Jh-SmHcqh1dcygYsENEv8UAXDIdpo",
+  authDomain: "studio-4815316560-21c29.firebaseapp.com",
+  projectId: "studio-4815316560-21c29",
+  storageBucket: "studio-4815316560-21c29.firebasestorage.app",
+  messagingSenderId: "816912441965",
+  appId: "1:816912441965:web:c737de51a4510f50379564",
+  measurementId: "G-ZWP5JPQ7KY"
 };
